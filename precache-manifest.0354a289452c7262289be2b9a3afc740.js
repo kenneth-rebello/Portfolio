dc977f334d9f0950c77574fec0bd21104ef6925e
Kenneth Rebello <54711676+kenneth-rebello@users.noreply.github.com>
2019-12-18 07:21:51 +0530

    Updates

---
+++ precache-manifest.0354a289452c7262289be2b9a3afc740.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "925f3b2d817d3401792aa04f7d54b2a1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "18bbe1dd5cf05a061f3c",
+    "url": "/portfolio/static/css/main.d382081c.chunk.css"
+  },
+  {
+    "revision": "66e3897e4965fe7bd20a",
+    "url": "/portfolio/static/js/2.4ed90b15.chunk.js"
+  },
+  {
+    "revision": "608ba5ece62cd2c14649ef57eba241d6",
+    "url": "/portfolio/static/js/2.4ed90b15.chunk.js.LICENSE"
+  },
+  {
+    "revision": "18bbe1dd5cf05a061f3c",
+    "url": "/portfolio/static/js/main.cf510bd7.chunk.js"
+  },
+  {
+    "revision": "8575fab3f3395c90d845",
+    "url": "/portfolio/static/js/runtime-main.680679b4.js"
+  },
+  {
+    "revision": "f55ba638287b768bee47aa91a12531a7",
+    "url": "/portfolio/static/media/KennethsResume.f55ba638.pdf"
+  },
+  {
+    "revision": "ec0f445160e2baef1a08bae39eb20503",
+    "url": "/portfolio/static/media/MERN.ec0f4451.png"
+  },
+  {
+    "revision": "c5df2b9d14a174e29bb19c5677671b4d",
+    "url": "/portfolio/static/media/Transmission.c5df2b9d.png"
+  },
+  {
+    "revision": "313435e48bece81fdd5e2278695a4be2",
+    "url": "/portfolio/static/media/dashphoto.313435e4.jpg"
+  },
+  {
+    "revision": "bf35584fa3ac15c3b6b8993eca3d514f",
+    "url": "/portfolio/static/media/kr.bf35584f.jpeg"
+  }
+]);
